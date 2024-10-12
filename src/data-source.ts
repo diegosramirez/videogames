7e18b3db2ea628d497f6f81e992c9fb358a0a496
Diego Sebastian Ramirez <dsramirez@pcugroup.com>
2024-10-12 15:46:37 -0300

    Add GitHub Actions workflow for EB deployment

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,11 +2,11 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'user',
-  password: 'password',
-  database: 'videogames',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
   synchronize: false,
